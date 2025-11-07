ec932be8fb04a5ad83111d575def355d0675dac4
Diana Oliveira <dianaoliveirandrade@gmail.com>
2025-11-07 11:04:07 -0300

    Refactor Usuario class and change ID generation strategy

--- atividade-login-produto/src/main/java/com/login/exemplo/entity/Usuario.java
+++ atividade-login-produto/src/main/java/com/login/exemplo/entity/Usuario.java
@@ -10,56 +10,54 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "tb_usuario")
 public class Usuario {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE)
-	private int id;
-	private String name;
-	@Column(unique = true)
-	private String email;
-	private String password;
-	
-	public Usuario(String name, String email, String password) {
-		this.name = name;
-		this.email = email;
-		this.password = password;
-	}
-	
-	public Usuario() {
-		
-	}
 
-	public int getId() {
-		return id;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    @Column(unique = true)
+    private String email;
+    private String password;
 
-	public String getName() {
-		return name;
-	}
+    public Usuario() {
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public Usuario(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
