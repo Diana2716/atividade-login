4d9cdb5a36014cf19ed5fcecf969e8f0ae5b56bf
Diana Oliveira <dianaoliveirandrade@gmail.com>
2025-11-07 11:04:48 -0300

    Refactor ProdutoRepository by removing findAllById
    Removed unnecessary method and cleaned up the interface.

--- atividade-login-produto/src/main/java/com/login/exemplo/repository/ProdutoRepository.java
+++ atividade-login-produto/src/main/java/com/login/exemplo/repository/ProdutoRepository.java
@@ -2,12 +2,8 @@ package com.login.exemplo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.login.exemplo.entity.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-
-	Produto findAllById(Long id);
-
 }
